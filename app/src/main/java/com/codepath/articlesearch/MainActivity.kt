f8420bbd1d94d4dec9f4ca7fd3578cb74b03a71c
4Fola <70343084+4Fola@users.noreply.github.com>
2022-10-10 17:19:25 -0400

    Step IV: Transition between fragments with Bottom Navigation - Checkpoint V: Final

--- app/src/main/java/com/codepath/articlesearch/MainActivity.kt
+++ app/src/main/java/com/codepath/articlesearch/MainActivity.kt
@@ -1,17 +1,11 @@
 package com.codepath.articlesearch
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.fragment.app.Fragment
 import com.codepath.articlesearch.databinding.ActivityMainBinding
-import com.codepath.asynchttpclient.AsyncHttpClient
-import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.serialization.json.Json
-import okhttp3.Headers
-import org.json.JSONException
 
 fun createJson() = Json {
     isLenient = true
@@ -25,7 +19,7 @@ private const val TAG = "MainActivity/"
 class MainActivity : AppCompatActivity() {
    private lateinit var binding: ActivityMainBinding
 
-    private fun replaceFragment(articleListFragment: ArticleListFragment) {
+    private fun replaceFragment(articleListFragment: Fragment) {
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.article_frame_layout, articleListFragment)
@@ -39,7 +33,29 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         // Call helper method to swap the FrameLayout with the fragment
-        replaceFragment(ArticleListFragment())
+        //replaceFragment(ArticleListFragment())
+        val fragmentManager = supportFragmentManager
+
+        // Fragment definition
+        val bestSellerBooksFragment: Fragment = BestSellerBooksFragment()
+        val articleListFragment: Fragment = ArticleListFragment()
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        // Navigation selection declaration
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            lateinit var fragment: Fragment
+            when (item.itemId) {
+                R.id.nav_books -> fragment = bestSellerBooksFragment
+                R.id.nav_articles -> fragment = articleListFragment
+            }
+            replaceFragment(fragment) // Change parameter to article list fragment if you get an error
+            true
+        }
+
+        // Initializing the default selection
+        bottomNavigationView.selectedItemId = R.id.nav_books
+
     }
 }
 
